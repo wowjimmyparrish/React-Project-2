8ffc958d5389e63a2815df815d2196dd642126c9
James Parrish <jthomasparrishjr@gmail.com>
2022-10-09 13:45:29 -0500

    updated cats db.json

--- src/Components/Cats.js
+++ src/Components/Cats.js
@@ -1,7 +1,8 @@
 function Cats() {
   return (
-    <div id="womens">
+    <div id="cats">
       <h1>Cats Component</h1>
+      <img src="https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
     </div>
   );
 }
--- src/Components/Dogs.js
+++ src/Components/Dogs.js
@@ -1,7 +1,8 @@
 function Dogs() {
   return (
-    <div id="mens">
+    <div id="dogs">
       <h1>Dogs Component</h1>
+      <img src="https://images.pexels.com/photos/733416/pexels-photo-733416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
     </div>
   );
 }
