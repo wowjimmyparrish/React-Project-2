ed1f6c955e19a9d6d152471d6798858ece803f34
James Parrish <jthomasparrishjr@gmail.com>
2022-10-10 19:02:50 -0500

    updated cat buttons

--- src/Components/CatCard.js
+++ src/Components/CatCard.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
+
 function CatCard({ cat }) {
+  const [showCatDetails, setShowCatDetails] = useState(false);
+
+  function handleCatClick() {
+    setShowCatDetails((showCatDetails) => !showCatDetails);
+  }
+
   return (
     <div>
       <li>
         <h2>{cat.name}</h2>
         <img src={cat.image} alt={cat.name}></img>
-        <div>
-          <p>Description: {cat.description}</p>
-          <p>Gender: {cat.gender}</p>
-          <p>Age: {cat.age}</p>
-          <button>Adopt</button>
-        </div>
+        <button onClick={handleCatClick}> About Me!!</button>
+        {showCatDetails ? (
+          <div>
+            <p>Description: {cat.description}</p>
+            <p>Gender: {cat.gender}</p>
+            <p>Age: {cat.age}</p>
+          </div>
+        ) : (
+          <p></p>
+        )}
       </li>
     </div>
   );
