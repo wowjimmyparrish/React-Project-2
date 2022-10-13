1cb82bae3e263e53531cc4fd9beca3fc7b71c59d
James Parrish <jthomasparrishjr@gmail.com>
2022-10-12 20:15:36 -0500

    updated about me

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,17 @@ function Home() {
     <div className="home">
       <h1>Schrodinger & Pavlov Pet Adoptions</h1>
       <h2>ABOUT US</h2>
+      <p>
+        Schrodinger & Pavlov was established in 2002 to help abused, abandoned,
+        and neglected animals. Started on a non-existent budget by two animal
+        lovers committed to a no-kill policy, the organization soon became an
+        important presence in the South Louisiana area and then beyond. Our
+        organization places approximately 1,500 pets in loving homes each year.
+        Schrodinger & Pavlov is known statewide for its many unique and
+        heart-warming success stories. Every animal that comes into Schrodinger
+        & Pavlov's care receives the best routine and specialized veterinary
+        care.
+      </p>
     </div>
   );
 }
